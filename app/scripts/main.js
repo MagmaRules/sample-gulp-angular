f97b50682fde5d2c857ced685e178413e6cd043d
Coxo <coxom@dub-mac015007.local>
2016-01-18 16:58:07 +0000

    Formatted main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,77 +1,85 @@
 var searchApp = angular.module('searchApp', []);
-searchApp.controller('SearchCtrl', ['$scope', '$http','$timeout', function ($scope, $http, $timeout) {
+searchApp.controller('SearchCtrl', ['$scope', '$http','$timeout',
+function($scope, $http, $timeout) {
+  var lastQuery = '';
+  $scope.query = '';
+  $scope.results = [];
 
-    var lastQuery = "";
-    $scope.query = "";
+  function zeroPad(num, places) {
+    var zero = places - num.toString().length + 1;
+    return Array(+(zero > 0 && zero)).join('0') + num;
+  }
+
+  function processResults(results) {
     $scope.results = [];
-
-    function zeroPad(num, places) {
-        var zero = places - num.toString().length + 1;
-        return Array(+(zero > 0 && zero)).join("0") + num;
+    for (var i in results) {
+      var entry = results[i];
+      entry.name = entry.name.toUpperCase();
+      // jscs:disable
+      var date = new Date(entry.end_of_an_era);
+      // jscs:enable
+      entry.endOfAnEra = zeroPad(date.getDate(),2) + '/' +
+      zeroPad(date.getMonth() + 1, 2) +
+      '/' + zeroPad(date.getFullYear().toString(), 4);
+      $scope.results.push(entry);
     }
+    endLoading();
+  }
 
-    function processResults(results){
-        $scope.results = [];
-        for(var i in results){
-            var entry = results[i];
-            entry.name = entry.name.toUpperCase();
-            var date = new Date(entry.end_of_an_era);
-            entry.endOfAnEra = zeroPad(date.getDate(),2) + "/" + zeroPad(date.getMonth() + 1, 2) + "/" + zeroPad(date.getFullYear().toString(), 4);
-            $scope.results.push(entry);
-        }
-        endLoading();
+  function loadResults(data) {
+    if (data && data.ancients) {
+      processResults(data.ancients);
+    } else {
+      processResults(data);
     }
+  }
 
-    function loadResults(data){
-        if(data && data.ancients){
-            processResults(data.ancients);
-        } else {
-            processResults(data);
-        }
+  function showLoading() {
+    $scope.loading = true;
+  }
+
+  function endLoading() {
+    $scope.loading = false;
+  }
+
+  showLoading();
+  $http.get('https://athena-7.herokuapp.com/ancients.json')
+  .success(function(data) {
+    processResults(data);
+  });
+
+  var tmpQuery = '';
+  var queryTimeout = null;
+
+  $scope.$watch('query', function(query) {
+    if (queryTimeout) {
+      $timeout.cancel(queryTimeout);
     }
+    tmpQuery = query;
+    queryTimeout = $timeout(function() {
+      $scope.search(tmpQuery);
+    }, 250);
+  });
 
-    function showLoading(){
-        $scope.loading = true;
-    }
-
-    function endLoading(){
-        $scope.loading = false;
-    }
-
+  $scope.showError = function() {
     showLoading();
-    $http.get('https://athena-7.herokuapp.com/ancients.json').success(function(data) {
-        processResults(data);
+    $http.get('https://athena-7.herokuapp.com/ancients.json?error=true')
+    .error(function(data) {
+      $scope.specialError = data.error;
+      endLoading();
     });
+  };
 
-
-    var tmpQuery = '';
-    var queryTimeout = null;
-
-    $scope.$watch('query', function (query) {
-        if (queryTimeout) $timeout.cancel(queryTimeout);
-        tmpQuery = query;
-        queryTimeout = $timeout(function() {
-            $scope.search(tmpQuery);
-        }, 250);
+  $scope.search = function(query) {
+    $scope.specialError = null;
+    if (query == lastQuery) {
+      return;
+    }
+    showLoading();
+    $http.get('https://athena-7.herokuapp.com/ancients.json?search=' + query)
+    .success(function(data) {
+      loadResults(data);
     });
-
-    $scope.showError = function(){
-        showLoading();
-        $http.get('https://athena-7.herokuapp.com/ancients.json?error=true').error(function(data) {
-            $scope.specialError = data.error;
-            endLoading();
-        });
-    };
-
-    $scope.search = function(query){
-        $scope.specialError = null;
-        if(query == lastQuery){
-            return;
-        }
-        showLoading();
-        $http.get('https://athena-7.herokuapp.com/ancients.json?search=' + query).success(function(data) {
-            loadResults(data);
-        });
-        lastQuery = query;
-    };
+    lastQuery = query;
+  };
 }]);
